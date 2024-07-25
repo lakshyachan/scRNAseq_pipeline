b8a471c02a077d9c3e1ba5dc52e0cbc60c63d4e7
Lakshmi Chanemougam <66237905+lakshyachan@users.noreply.github.com>
2024-07-25 19:37:22 -0400

    Delete GitHub_code directory

--- GitHub_code/code/get_cuomo_data/md5sums/checkmd5sums.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd data
-cd cram_ftps/
-for i in `find . -type f`; do md5sum $i >> cram_md5s.txt; done
-cut -d ' ' -f 1 downloaded_cram_md5.txt
-rm cram_md5s.txt
-diff downloaded_cram_md5.txt all_crams_md5.txt
